923e29f89b106f157158b9cd67bcd2ec0c1f7fbd
1juyoung <myjuyoung2@gmail.com>
2025-04-15 16:57:28 +0900

    feat #16 이메일 입력 영역 수정 - 이메일 input 필드 조정 - 중복확인 버튼 크기 및 위치 조정 - 확인 버튼에서 다음 버튼으로 수정 - 로그인 링크 위치 조정

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -5,21 +5,20 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button"
 
 export default function SignUpPage() {
-  const Style = {
-    Input: "w-90 h-11 m-2 md:w-90 bg-gray-100",
-    Link: "flex self-end mr-10 font-medium",
-  };
 
   return (
     <div className="justify-center items-center flex flex-col h-screen w-full">
       <div className="font-bold text-lg">회원가입</div>
-      <Input type="email" placeholder="이메일을 입력하세요." className={Style.Input} />
-      <Button variant="outline" className="w-100 h-11 m-2 md: w-90">중복확인</Button>
-
-      <Input type="password" placeholder="비밀번호를 입력하세요." className={Style.Input} />
-      <Button variant="ghost" className="w-100 h-11 m-2 bg-blue-900 text-white md: w-90">확인</Button>
+      <div className="flex items-center">
+        <Input type="email" placeholder="이메일을 입력하세요."
+          className="flex w-65 h-11 m-2
+          md:w-65 bg-gray-100" />
+        <Button variant="outline" className="w-30 h-11 m-2 md: w-20">중복확인</Button>
+      </div>
+      <Input type="password" placeholder="비밀번호를 입력하세요." className="w-90 h-11 m-2 md:w-90 bg-gray-100" />
+      <Button variant="ghost" className="w-100 h-11 m-2 bg-blue-900 text-white md: w-90">다음</Button>
       <Button variant="ghost" className="w-100 h-11 m-2 bg-amber-200 md: w-90">카카오 회원가입</Button>
-      <Link href="/auth/login" className={Style.Link}>로그인</Link>
+      <Link href="/auth/login" className="font-medium">로그인</Link>
     </div>
   );
 }
