3344d7bae07fd412b061c89db1cfe6111b1c3aa1
dev-vming <dev.vming@gmail.com>
2025-04-18 22:14:34 +0900

    fix: 쿠키 관련 내용 삭제

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -2,19 +2,13 @@ import { SbUserRepository } from './../../../../infra/repositories/supabase/SbUs
 import { EmailLoginUsecase } from './../../../../application/usecases/auth/EmailLoginUsecase';
 import { NextRequest, NextResponse } from 'next/server';
 import { EmailLoginRequestDto } from '@/application/usecases/auth/dto/AuthDto';
-import { setAuthCookie } from '@/utils/cookie';
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-
     const { email, password, remember } = body as EmailLoginRequestDto & { remember: boolean };
-
     const { user, token } = await EmailLoginUsecase(SbUserRepository(), { email, password });
-
     const res = NextResponse.json({ user }, { status: 200 });
-
-    setAuthCookie(res, token, remember);
 
     return res;
   } catch (error: any) {
