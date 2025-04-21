911dcb5498bc64945b61c5ab0cfdca291ff3eff6
minho <kasamin98@naver.com>
2025-04-22 00:07:57 +0900

    chore: #25 tanstack-query 설치 및 provider세팅

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,26 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Navbar } from './components/NavBar';
-
+import QueryProviders from '@/providers/QueryProvider';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 export const metadata: Metadata = {
   title: 'Eat The Weather',
   description: '날씨를 잇다, 잇더웨더',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="ko">
       <body className="bg-gradient-to-b from-sky-100 to-white min-h-screen flex flex-col items-center pt-[env(safe-area-inset-top)]">
         <div className="w-full max-w-md bg-white min-h-screen shadow-md relative z-10 ">
-          {children}
+          <QueryProviders>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryProviders>
         </div>
         <Navbar />
       </body>
