9b8012a6f9bce04ced71f7e6fbf4cad2434b461e
1juyoung <myjuyoung2@gmail.com>
2025-04-23 16:50:03 +0900

    feat: #44 OpenWeatherMap API - 현재 날씨 - type.ts 데이터 타입 정의 - useWeather.ts CurrentWeather Api 추가

--- lib/hooks/useWeather.ts
+++ lib/hooks/useWeather.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-import { HourlyWeatherResponse } from '../types/weather';
+import { HourlyWeatherResponse, CurrentWeatherResponse } from '../types/weather';
 
 const API_KEY = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY;
 
@@ -17,6 +17,22 @@ export const useWeather = (lat?: number, lon?: number) => {
     },
     enabled: !!lat && !!lon,
     staleTime: 1000 * 60 * 60, // 1시간 캐시
+    refetchOnWindowFocus: false,
   });
 };
 
+export const useCurrentWeather = (lat?: number, lon?: number) => {
+  return useQuery<CurrentWeatherResponse>({
+    queryKey: ['homeWeather', lat, lon],
+    queryFn: async () => {
+      const res = await axios.get<CurrentWeatherResponse>(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`
+      );
+      console.log('홈 날씨 API 응답', res.data);
+      return res.data;
+    },
+    enabled: !!lat && !!lon,
+    staleTime: 1000 * 60 * 60, // 1시간 캐시
+    refetchOnWindowFocus: false,
+  });
+}
--- lib/types/weather.ts
+++ lib/types/weather.ts
@@ -1,5 +1,3 @@
-// lib/types/weather.ts
-
 export type HourlyWeatherEntry = {
     dt_txt: string;
     main: {
@@ -36,4 +34,46 @@ export type HourlyWeatherEntry = {
       sunset: number;
     };
   };
+
+export interface CurrentWeatherResponse {
+    coord: {
+      lon: number;
+      lat: number;
+    };
+    weather: {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }[];
+    main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      humidity: number;
+      sea_level?: number;
+      grnd_level?: number;
+    };
+    wind: {
+      speed: number;
+      deg: number;
+      gust?: number;
+    };
+    clouds: {
+      all: number;
+    };
+    rain?: {
+      '1h': number;
+    };
+    dt: number;
+    sys: {
+      country: string;
+      sunrise: number;
+      sunset: number;
+    };
+    name: string;
+  }
+  
   
