4d3200999021d7e8ce790af8c9311de52c1ce358
dev-vming <dev.vming@gmail.com>
2025-04-25 00:52:23 +0900

    feat: 마이페이지에서 계정 정보 수정할 수 있는 버튼 UI 변경

--- app/member/page.tsx
+++ app/member/page.tsx
@@ -5,13 +5,13 @@ import LinkItem from '../components/LinkItem';
 import { useUserStore } from '@/store/userStore';
 import { useRouter } from 'next/navigation';
 import { useOnboardingStore } from '@/store/onboardingStore';
+import { SquarePen } from 'lucide-react';
 
 const linkItems = [
   { href: '/member/posts', label: '작성한 게시물' },
   { href: '/member/likes', label: '좋아요한 게시물' },
   { href: '/onboarding', label: '날씨 민감도 관리' },
   { href: '/member/regions', label: '지역 즐겨찾기 관리' },
-  { href: '/member/infos', label: '계정 관리' },
   { href: '', label: '로그아웃' },
 ];
 
@@ -37,17 +37,23 @@ export default function ProfilePage() {
   return (
     <div className="min-h-screen px-4 py-6 bg-white flex flex-col">
       <h1 className="text-lg font-bold mb-6">마이페이지</h1>
-      <div className="flex items-center gap-4 mb-8 mt-10">
-        <Avatar className="w-12 h-12">
-          <AvatarImage
-            src={user.profile_image || '/images/user2.png'}
-            alt="유저 아바타"
-          />
-          <AvatarFallback>프로필</AvatarFallback>
-        </Avatar>
-        <div>
-          <p className="font-semibold text-base">{user.nickname}</p>
-          <p className="text-gray-400 text-sm">{user.email}</p>
+      <div className="flex items-center mb-8 mt-40 justify-between">
+        <div className='flex items-center gap-4'>
+          <Avatar className="w-12 h-12">
+            <AvatarImage
+              src={user.profile_image || '/images/user2.png'}
+              alt="유저 아바타"
+            />
+            <AvatarFallback>프로필</AvatarFallback>
+          </Avatar>
+          <div>
+            <p className="font-semibold text-base">{user.nickname}</p>
+            <p className="text-gray-400 text-sm">{user.email}</p>
+          </div>
+        </div>
+
+        <div className="mr-5">
+          <SquarePen className="text-gray-700 w-5 h-5" />
         </div>
       </div>
 
@@ -55,10 +61,7 @@ export default function ProfilePage() {
         {linkItems.map((item) =>
           item.label === '로그아웃' ? (
             <div onClick={handleLogout} key={item.href + item.label}>
-              <LinkItem
-                href={item.href}
-                label={item.label}
-              />
+              <LinkItem href={item.href} label={item.label} />
             </div>
           ) : (
             <LinkItem
